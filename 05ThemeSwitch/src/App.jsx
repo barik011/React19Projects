99aef3d13c4fb52e72c94fdd2138ab615e324e2c
Github Projects <barik011@gmail.com>
2026-09-03 16:27:11 +0530

    Theme Switch

--- 05ThemeSwitch/src/App.jsx
+++ 05ThemeSwitch/src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
+import React,{useEffect,useState} from 'react'
 import './App.css'
+import { ThemeProvider } from './context/theme'
+import SwitchBtn from './components/SwitchBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [themeMode,setThemeMode]=useState('light')
+  const darkMode =()=>{
+    setThemeMode('dark');
+  }
+  const lightMode=()=>{
+     setThemeMode('light');
+  }
+
+
+  useEffect(()=>{
+    document.documentElement.classList.remove('light','dark');
+    document.documentElement.classList.add(themeMode);
+  },[themeMode])
 
   return (
-    <div className='flex w-screen h-screen'>
-      <h1>Theme Switch Using Context API</h1>
-    </div>
+    <ThemeProvider value={{themeMode,darkMode,lightMode}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <SwitchBtn />
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card />
+                    </div>
+                </div>
+            </div>
+    </ThemeProvider>
   )
 }
 
