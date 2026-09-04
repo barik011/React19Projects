c8d734c9460d840690f6a64c65d44bb8122d59ad
Github Projects <barik011@gmail.com>
2026-09-04 17:56:30 +0530

    New Project for TODO

---
+++ 06TodoApp/src/components/TodoList.jsx
@@ -0,0 +1,38 @@
+function TodoList({ todo }) {
+    
+
+    return (
+        <div
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black"
+            }`}
+        >
+            <input
+                type="checkbox"
+                className="cursor-pointer"
+                
+            />
+            <input
+                type="text"
+                className={`border outline-none w-full bg-transparent rounded-lg`}
+                value="Todo list 1"
+                readOnly={true}
+            />
+            {/* Edit, Save Button */}
+            <button
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                
+                disabled={true}
+            >
+                {true ? "📁" : "✏️"}
+            </button>
+            {/* Delete Todo Button */}
+            <button
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+            >
+                ❌
+            </button>
+        </div>
+    );
+}
+
+export default TodoList;
