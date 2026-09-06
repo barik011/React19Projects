b33fe654295a3495a518dd406b367992c908a3d2
Github Projects <barik011@gmail.com>
2026-09-06 15:42:43 +0530

    New Changes

--- 07ReduxToolKitTodo/src/App.jsx
+++ 07ReduxToolKitTodo/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
+import TodoForm from '../components/TodoForm'
+import TodosList from '../components/TodosList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className='w-full h-full flex justify-center item-center'></div>
+    <>    
+    <div className='flex flex-col justify-center items-center'>
       <h1 className=''>Todo Project Using React Redux Toolkit</h1>
+      <TodoForm />
+      <TodosList /> 
+      </div>    
     </>
   )
 }
