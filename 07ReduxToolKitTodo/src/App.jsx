8f6900d6f2e348a36ab61b1c24fcee9447f2a9bd
Github Projects <barik011@gmail.com>
2026-09-08 20:24:01 +0530

    Title Changes

--- 07ReduxToolKitTodo/src/App.jsx
+++ 07ReduxToolKitTodo/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>    
     <div className='flex flex-col justify-center items-center w-full py-5 mt-10'>
-      <h1 className=''>Todo Project Using React Redux Toolkit</h1>
+      <h1 className='text-5xl text-fuchsia-950 mb-3'>Todo Project Using React Redux Toolkit</h1>
       <TodoForm />
       <TodosList /> 
       </div>    
