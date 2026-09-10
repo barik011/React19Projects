6570ba48a269d2d5da7162600e046e5055bb606c
Github Projects <barik011@gmail.com>
2026-09-10 14:29:29 +0530

    Auth Services Done

---
+++ 08BlogPostProject/src/appwrite/auth-services.js
@@ -0,0 +1,60 @@
+import conf from '../conf/conf'
+import {Client,Account,ID} from 'appwrite'
+
+export class AuthServ { 
+
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteURL)
+        .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create({id:ID.unique(),email,password,name});
+            if(userAccount){
+                this.login({email,password})
+            }
+            else{
+                return userAccount;
+            }
+        } catch (error) {
+            //console.log("Create Account Method Error: ", error);
+        }
+
+    }
+
+    async login({email,password}) {
+        try {
+            return await this.account.createEmailPasswordSession({email,password})
+        } catch (error) {
+            console.log("Login Method Error: ", error)
+        }
+
+    }
+    async getCurrentUser(){
+        try {
+            const session = await this.account.get()
+        } catch (error) {
+            console.log("getCurrentUser Method Error: ", error)
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions
+            
+        } catch (error) {
+            console.log("Logout Method Error: ",error)
+            
+        }
+    }
+}
+
+const authServ = new AuthServ();
+
+export default authServ;
