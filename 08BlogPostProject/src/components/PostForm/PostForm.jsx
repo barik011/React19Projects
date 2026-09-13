247ccab7f0afdbff8e3efce553fb0b0a50806986
Github Projects <barik011@gmail.com>
2026-09-13 17:13:59 +0530

    Changes

--- 08BlogPostProject/src/components/PostForm/PostForm.jsx
+++ 08BlogPostProject/src/components/PostForm/PostForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import {useForm} from 'react-hook-form'
 import {Input,Button,RTE} from '../components'
 import aapwriteServ from '../appwrite/appwrite-services'
@@ -13,7 +13,49 @@ const PostForm = (post) => {
             slug:post?.slug||'',
             status:post?.status||'Active'
         }
-    })
+    });
+    const navigate = useNavigate();
+    const authData = useSelector((state)=>(state.auth.userData));
+
+    const submit =async (data)=>{
+        if(post){
+            const file = data.image[0]? await aapwriteServ.uploadFile(data.image[0]):null;
+            if(file){
+                aapwriteServ.deleteFile(featuredImage)
+            }
+
+            const dbPost = await aapwriteServ.updatePost(post.$id,{...data,featuredImage:file?file.$id:undefined});
+            if(dbPost){
+                navigate(`/post/${dbPost.$id}`);
+            }
+        }
+        else{
+            const file = await aapwriteServ.uploadFile(data.image[0])
+
+            if(file){
+              const fileId =  file.$id
+              data.featuredImage= fileId
+
+              const dbPost = await aapwriteServ.updatePost({...data,userId:userData.$id});
+              if(dbPost){
+                navigate(`/post/${dbPost.$id}`);
+            }
+
+            }
+        }
+    }
+
+    const slugTransform = useCallback((value)=>{
+        if(value && typeof value === 'string'){
+            return value
+            .trim()
+            .toLowerCase()
+            .replace(/^[a-zA-Z\d\s]+/, '-')
+            .replace(/\s/g,'-')
+        }
+        return '';
+    },[])
+
   return (
     <div>PostForm</div>
   )
